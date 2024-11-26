f4d09342aa144551fdec4997d8d663dbec50b5e8
Danielroxs <danroxsll@gmail.com>
2024-11-25 19:56:40 -0600

    POST axios method

--- axios/app.js
+++ axios/app.js
@@ -1,3 +1,7 @@
+// -------------------------------------------------------------------------------- //
+
+// Metodo GET
+
 axios.get('https://jsonplaceholder.typicode.com/posts')
     .then(response => {
         console.log(response.data); // muestra los datos obtenidos
@@ -25,3 +29,56 @@ document.getElementById('fetchData').addEventListener('click', () => {
             console.error('Error:', error);
         })
 })
+
+// -------------------------------------------------------------------------------- //
+
+// Metodo POST
+
+axios.post('https://jsonplaceholder.typicode.com/posts', {
+    title: 'Nuevo post',
+    body: 'Este es un nuevo post creado con axios',
+    userId: 1
+})
+    .then(response => {
+        console.log('Datos enviado:', response.data);
+    })
+    .catch(error => {
+        console.error('Error al enviar datos:', error);
+    })
+
+document.getElementById('postForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value
+    const body = document.getElementById('body').value
+
+    axios.post('https://jsonplaceholder.typicode.com/posts', {
+        title: title,
+        body: body,
+        userId: 1
+    })
+        .then(response => {
+            console.log('Datos enviados:', response.data);
+            alert('Datos enviados correctamente');
+        })
+        .catch(error => {
+            console.error('Error al enviar datos:', error);
+        })
+})
+
+axios.post('https://jsonplaceholder.typicode.com/posts', {
+    title: 'Nuevo post',
+    body: 'Este es un nuevo post creado con axios',
+    userId: 1
+}, {
+    headers: {
+        'Authorization': 'Bearer mi-token-de-prueba',
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => {
+        console.log('Datos enviados con headers:', response.data);
+    })
+    .catch(error => {
+        console.error('Error al enviar datos con headers:', error);
+    })
